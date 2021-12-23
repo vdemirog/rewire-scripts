0bc6ee661a2ec9bfe9b3381d26317e3f9f5ac06c
vdemirog <vasilisdemiroglou@gmail.com>
2021-12-23 18:11:55 +0200

    freshness period changed

--- producer.sh
+++ producer.sh
@@ -6,7 +6,7 @@ for k in $DB_SENSORS
 do 
 	for i in $(seq 0 10);
 	do
-		echo 'RANDOM' | ndnpoke -x 2000 -w 7200000 ndn:/example/$k$i &
+		echo 'RANDOM' | ndnpoke -x 60000 -w 7200000 ndn:/example/$k$i &
 	done
 	sleep 3	
 done
