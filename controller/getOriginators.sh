aaadd2b264f4505364d9f7e149a9488a3e4556fb
vdemirog <vasilisdemiroglou@gmail.com>
2022-01-04 19:56:21 +0200

    controller scripts added

---
+++ controller/getOriginators.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true; do
+    sudo alfred -r 65 > bestoriginators
+sleep 1;
+done
+
--- listen.sh
+++ listen.sh
@@ -5,7 +5,7 @@ do
         result=$(nc -l -p 8000)
         echo $result
         firstcmd=`echo "${result}" | head -1`
-        secondcmd=`echo "${result}" | head -2`
+        secondcmd=`echo "${result}" | head -2 | tail -1`
         ./ndnconf.sh $firstcmd
         ./ndnconf.sh $secondcmd
 
