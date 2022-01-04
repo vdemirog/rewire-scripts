adeb96e861a1d2fc0007665ab23e73b75b60a380
vdemirog <vasilisdemiroglou@gmail.com>
2022-01-04 12:38:09 +0200

    consumer and producer scripts updated

--- producer.sh
+++ producer.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-DB_SENSORS="hum temp sound light movement hall"
+# 150 content objects
+content="1 2 3 4 5 6 7 8 9 10 11 12 38413 14 15 16 17 18 13 20 21 22 23 24 25 26 27 19 29 30 31 32 33 544 35 36 37 39 40 41 42 43 44 45 46 47 48 49 52 53 54 55 59 61 63 64 67 1092 69 70 71 72 74 76 77 78 85 86 88 4185 90 89 604 261720 95 102 103 111 114 123 126 130 134 142 143 659 148 147 2197 1175 149 155 156 158 159 160 171 173 174 687 179 181 183 15034 188 194 200 215 218 221 3818 235 749 243 757 18167 270 289 2342 2348 816 20785 316 318 319 320 2881 43333 1372 8039 1388 367 385 31618 388 906 2450 408 416 936 4796851 1472 1481 972 465 467 5605 4072 3563 2037"
 
-for k in $DB_SENSORS 
+for k in $content
 do 
-	for i in $(seq 0 10);
-	do
-		echo 'RANDOM' | ndnpoke -x 60000 -w 7200000 ndn:/example/$k$i &
-	done
-	sleep 3	
+	echo $RANDOM | ndnpoke -x 10000 -w 7200000 ndn:/content$k &
+    sleep 1	
 done
 
 while true 
 do
-	for k in $DB_SENSORS 
+	for k in $content 
 	do 
-		for i in $(seq 0 10);
-		do
-			if ! pgrep  -f "/example/$k$i" > /dev/null 
-			then
-				echo 'RANDOM' | ndnpoke -x 60000 -w 7200000 ndn:/example/$k$i & 
-			fi
-		done
+		if ! pgrep  -f "/content$k" > /dev/null 
+		then
+			echo $RANDOM | ndnpoke -x 10000 -w 7200000 ndn:/content$k & 
+		fi
 	done
 done
 
