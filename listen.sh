b6fd76b7e3d8ba8e8af1e6e94dd8b09a11b65af7
vdemirog <vasilisdemiroglou@gmail.com>
2022-01-06 17:45:58 +0200

    scripts fixed

--- listen.sh
+++ listen.sh
@@ -3,12 +3,9 @@
 while true;
 do
         result=$(nc -l -p 8000)
-        echo $result
         firstcmd=`echo "${result}" | head -1`
         secondcmd=`echo "${result}" | head -2 | tail -1`
-        ./ndnconf.sh $firstcmd
-        ./ndnconf.sh $secondcmd
-
+	    docker exec ndn sh -c "$firstcmd; $secondcmd"
 done
 exit 0
 
